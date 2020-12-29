081b4e46c040e77311695a062ca035ec3e9ae330
Piotr Wojtyniak <33482377+HenryTy@users.noreply.github.com>
2020-12-29 14:15:28 +0100

    Skrypt tworzący tabele hurtowni danych

---
+++ hurtownia.scala
@@ -0,0 +1,71 @@
+spark.sql("DROP TABLE IF EXISTS `czas`")
+spark.sql("DROP TABLE IF EXISTS `typy_pojazdow`")
+spark.sql("DROP TABLE IF EXISTS `miejsca`")
+spark.sql("DROP TABLE IF EXISTS `pogoda`")
+spark.sql("DROP TABLE IF EXISTS `fakty`")
+
+spark.sql("""CREATE TABLE `czas` (
+ `id` int,
+ `rok` int,
+ `miesiac` int,
+ `data` date,
+ `godzina` int,
+ `kwartal` int,
+ `dzien_tygodnia` string)
+ROW FORMAT SERDE
+ 'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+STORED AS INPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat'
+OUTPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
+
+spark.sql("""CREATE TABLE `typy_pojazdow` (
+ `id` int,
+ `typ` string,
+ `kategoria` string,
+ `rodzaj` string)
+ROW FORMAT SERDE
+ 'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+STORED AS INPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat'
+OUTPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
+
+spark.sql("""CREATE TABLE `miejsca` (
+ `id` int,
+ `kod_ons_obszaru` string,
+ `nazwa_obszaru` string,
+ `nazwa_regionu` string,
+ `krolestwo` string,
+ `nazwa_drogi` string,
+ `kategoria_drogi` string,
+ `typ_drogi` string)
+ROW FORMAT SERDE
+ 'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+STORED AS INPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat'
+OUTPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
+
+spark.sql("""CREATE TABLE `pogoda` (
+ `id` int,
+ `opis_pogody` string)
+ROW FORMAT SERDE
+ 'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+STORED AS INPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat'
+OUTPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
+
+spark.sql("""CREATE TABLE `fakty` (
+ `id_czasu` int,
+ `id_pojazdu` int,
+ `id_miejsca` int,
+ `id_pogody` int,
+ `liczba_pojazdow` int)
+ROW FORMAT SERDE
+ 'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+STORED AS INPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat'
+OUTPUTFORMAT
+ 'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
