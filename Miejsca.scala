2b6d006941ffb8aded8581adc94999f63925a047
Lt04 <48619839+Lt04@users.noreply.github.com>
2021-01-16 20:53:39 +0100

    Add files via upload

---
+++ Miejsca.scala
@@ -0,0 +1,43 @@
+package com.example.bigdata
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.{row_number, col}
+import org.apache.spark.sql.expressions.Window
+
+object Miejsca {
+
+  def main(args: Array[String]) {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Miejsca")
+      .enableHiveSupport()
+      .getOrCreate()
+    import spark.implicits._
+
+    val regionyDS = spark.read.csv(args(0) + "/regions.csv")
+    val wladzeDS = spark.read.csv(args(0) + "/authorities.csv")
+    val drogiDS = spark.read.csv(args(0) + "/mainData.csv")
+    val razemDS = regionyDS.join(wladzeDS, wladzeDS("region_ons_code") === regionyDS("region_ons_code")).
+      select(
+        regionyDS("region_ons_code"),
+        wladzeDS("local_authority_name"),
+        regionyDS("region_name"),
+        wladzeDS("local_authority_ons_code")
+      )
+    val polaczone = razemDS.join(drogiDS, drogiDS("local_authority_ons_code") === razemDS("local_authority_ons_code")).
+      select(
+        razemDS("region_ons_code"),
+        razemDS("local_authority_name"),
+        razemDS("region_name"),
+        drogiDS("road_name"),
+        drogiDS("road_category"),
+        drogiDS("road_type")
+      )
+    val window = Window.orderBy(col("region_ons_code"))
+      weatherDS
+      .distinct()
+      .withColumn("id", row_number.over(window))
+      .write
+      .insertInto("miejsca")
+  }
+}
